3c50fb5c21bbd82419fd2cc307e091edd1660984
li_han <li_han@hismartinfo.com>
2021-01-22 10:41:48 +0800

    初始化项目

---
+++ cloud-alibaba-demo/src/main/java/com/CloudAlibabaDemo.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaDemo.class, args);
+    }
+}
